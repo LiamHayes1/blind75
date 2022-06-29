fa7975967936d984be7389055b847defd7cab0e7
LiamHayes1 <liamhayes@live.ie>
2022-06-29 16:51:12 +0100

    Update contains duplicate

--- src/arraysandhashing/ContainsDuplicate.java
+++ src/arraysandhashing/ContainsDuplicate.java
@@ -1,4 +1,14 @@
 package arraysandhashing;
 
 public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> counts = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (counts.contains(nums[i])) {
+                return true;
+            }
+            counts.add(nums[i]);
+        }
+        return false;
+    }
 }
