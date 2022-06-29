a8796c951560e6f9191ca6c2ab4217ba20cbd8f4
LiamHayes1 <liamhayes@live.ie>
2022-06-29 16:52:35 +0100

    Valid anagram

--- src/arraysandhashing/ContainsDuplicate.java
+++ src/arraysandhashing/ContainsDuplicate.java
@@ -1,5 +1,8 @@
 package arraysandhashing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> counts = new HashSet<>();
